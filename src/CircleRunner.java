80ebb00aada804641866a0c9bf672da71c3d6311
Jian Hung Ye <jianhungy@nycstudents.net>
2023-09-28 08:24:33 -0400

    Initial commit

---
+++ src/CircleRunner.java
@@ -0,0 +1,14 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(5);
+        circle1.printArea();
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+
+        // C. BEFORE running this code, PREDICT what gets printed
+
+        // D. run the code and confirm your prediction! Were you correct?
+    }
+}
+
